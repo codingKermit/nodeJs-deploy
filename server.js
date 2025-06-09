e05b4df0a10f6cb00c7a15f9656643f7bd993118
codingKermit <woojw6012@gmail.com>
2025-06-09 22:59:03 +0900

    test

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require('./app');
 
-app.listen(app.get('port'),()=>{
-    console.log(app.get('port'),'번 포트에서 대기 중');
-})
+app()
+    .then((app)=>{
+        app.listen(app.get('port'),()=>{
+            console.log(app.get('port'),'번 포트에서 대기 중');
+        })
+    })
+    .catch((err)=>{
+        console.error('서버 실행 오류',err);
+    })
