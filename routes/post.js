c0a2d52c03fc1c78660046a45b1e0a9217bd0cc5
codingKermit <woojw6012@gmail.com>
2025-06-15 18:46:46 +0900

    test

--- routes/post.js
+++ routes/post.js
@@ -10,10 +10,10 @@ const { S3Client } = require("@aws-sdk/client-s3");
 
 const s3 = new S3Client({
     credentials:{
-        accessKeyId : process.env.S3_ACCESS_KEY,
-        secretAccessKey : process.env.S3_SECRET_ACCESS_KEY
+        accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
     },
-    region: "ap-northeast-2",
+    region: process.env.AWS_REGION,
 });
 
 try {
